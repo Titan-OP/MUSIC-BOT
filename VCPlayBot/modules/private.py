407f172c17c638d15c74d970d401563b1bb87c25
тєcнησ✘ρяσ <83203983+Titan-OP@users.noreply.github.com>
2021-09-18 11:59:00 +0530

    Update private.py

--- VCPlayBot/modules/private.py
+++ VCPlayBot/modules/private.py
@@ -56,7 +56,7 @@ async def start_(client: Client, message: Message):
                          "📚 Commands", callback_data="cbcmds"
                     ),
                     InlineKeyboardButton(
-                        "💝 Donate", url=f"https://t.me/{OWNER_NAME}")
+                        "💝 Owner", url=f"https://t.me/{OWNER_NAME}")
                 ],[
                     InlineKeyboardButton(
                         "👥 Official Group", url=f"https://t.me/{SUPPORT_GROUP}"
